a05de1f394b5d8890ca591f4f9cd296146a53a3a
Denis Schlusche <denis.schlusche@web.de>
2024-08-03 18:54:18 +0200

    renamed main scene to "hub"

--- scipts/upgradable_hub_container.gd
+++ scipts/upgradable_hub_container.gd
@@ -4,10 +4,17 @@ class_name UpgradableHubContainer extends Resource
 @export var attribute_name: String
 @export var upgradables_list : Array[UpgradableHubLevel] = []
 
-func upgrade(current_level):
+func upgrade_level(current_level):
 	
 	if current_level>=upgradables_list.size():
 		push_error("Upgrade level already above maximum.")
-		return
+		return false
+		
+	
+	var hub_level : UpgradableHubLevel = upgradables_list[current_level]
+	if HubState.currency >= hub_level.cost:
+		HubState.currency -= hub_level.cost
+		return true
+	
 	pass
 
